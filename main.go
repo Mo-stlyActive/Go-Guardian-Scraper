94599f58d66ce85bb318a0d5743ccc2ea7e8d70d
Mo <moho2010@live.ie>
2025-03-17 16:15:13 -0400

    Project complete for now

--- main.go
+++ main.go
@@ -1,112 +1,137 @@
 package main
 
-import ("fmt"
-"github.com/PuerkitoBio/goquery"
-"math/rand"
-"net/http"
-"strings"
-"time"
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
 )
-userAgents = []string{
-	"rest"
+
+var userAgents = []string{
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+	"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Safari/604.1.38",
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Safari/604.1.38",
 }
 
-func randomUserAgent(){
- rand.Seed(time.Now().Unix)
- randomNum := rand.Int() %len(userAgents)
- return userAgents[randomNum]
+func randomUserAgent() string {
+	rand.Seed(time.Now().Unix())
+	randNum := rand.Int() % len(userAgents)
+	return userAgents[randNum]
 }
 
-func discoverLinks(response &http.Response, baseURL string) []string{
+func discoverLinks(response *http.Response, baseURL string) []string {
 	if response != nil {
 		doc, _ := goquery.NewDocumentFromResponse(response)
 		foundUrls := []string{}
-		if doc != nil{
-			doc.Find("a").Each(func(i int, s *goquery.Selection)){
-				res,_ := s.Attr("href")
+		if doc != nil {
+			doc.Find("a").Each(func(i int, s *goquery.Selection) {
+				res, _ := s.Attr("href")
 				foundUrls = append(foundUrls, res)
-			}
+			})
 		}
 		return foundUrls
-	} else{
+	} else {
 		return []string{}
 	}
 }
-func getRequest(targetURL string)(*http.Response, error){
+func getRequest(targetUrl string) (*http.Response, error) {
 	client := &http.Client{}
 
-	req,_err := http.NewRequest("GET", targetURL,nil)
-	if err != nil {
-		return nil, err
-	} else{
-		return res, nil
-	}
-
-	req.Header.Set("User-Agent", randomUserAgent)
+	req, _ := http.NewRequest("GET", targetUrl, nil)
+	req.Header.Set("User-Agent", randomUserAgent())
 
 	res, err := client.Do(req)
 
 	if err != nil {
 		return nil, err
-	} else{
+	} else {
 		return res, nil
 	}
 }
 
-func resolveRelativeLinks(){
+func checkRelative(href string, baseURL string) string {
+	if strings.HasPrefix(href, "/") {
+		return fmt.Sprintf("%s%s", baseURL, href)
+	} else {
+		return href
+	}
 
 }
 
-func Crawl(targetURL string, baseURL string) []string{
+func resolveRelativeLinks(href string, baseURL string) (bool, string) {
+	resultHref := checkRelative(href, baseURL)
+	baseParse, _ := url.Parse(baseURL)
+	resultParse, _ := url.Parse(resultHref)
+	if baseParse != nil && resultParse != nil {
+		if baseParse.Host == resultParse.Host {
+			return true, resultHref
+		} else {
+			return false, ""
+		}
+	}
+	return false, ""
+}
+
+var tokens = make(chan struct{}, 5)
+
+func Crawl(targetURL string, baseURL string) []string {
 	fmt.Println(targetURL)
+	tokens <- struct{}{}
 	response, _ := getRequest(targetURL)
+	<-tokens
 
 	links := discoverLinks(response, baseURL)
 	foundUrls := []string{}
 
-	for _ , link := range links{
+	for _, link := range links {
 		ok, correctLink := resolveRelativeLinks(link, baseURL)
 		if ok {
-			if correctLink != ""{
+			if correctLink != "" {
 				foundUrls = append(foundUrls, correctLink)
 			}
 		}
 	}
-	
+
 	// ParseHTML(response)
 	return foundUrls
 }
 
 // func ParseHTML(response *http.Response){
-	//Can take the information and parse it. maybe use it to get headlines or articles on certain topics? future project idea. 
-	//Tutorial 32 explains parsing results
+//Can take the information and parse it. maybe use it to get headlines or articles on certain topics? future project idea.
+//Tutorial 32 explains parsing results
 // }
 
 func main() {
 	worklist := make(chan []string)
 	var n int
 	n++
-	baseDomain := "https://www.theguardian.com"
-	go func(){worklist <- []string{"https://www.theguardian.com"}}()
+	baseDomain := "https://www.theacademyofcode.com/"
+	go func() { worklist <- []string{"https://www.theacademyofcode.com/"} }()
 
 	seen := make(map[string]bool)
 
-	for; n>0; n--{
+	for ; n > 0; n-- {
 
-	
-	list := worklist
+		list := <-worklist
 
-	for _, link := range list{
-		if !seen[link]{
-			seen[link] = true
-			n++
-			go func(link string, baseURL string){
-				foundLinks := Crawl(link, baseDomain)
-				if foundLinks != nil{
-					worklist <- foundLinks
-				}
-			} 
+		for _, link := range list {
+			if !seen[link] {
+				seen[link] = true
+				n++
+				go func(link string, baseURL string) {
+					foundLinks := Crawl(link, baseDomain)
+					if foundLinks != nil {
+						worklist <- foundLinks
+					}
+				}(link, baseDomain)
+			}
 		}
 	}
-}
 }
