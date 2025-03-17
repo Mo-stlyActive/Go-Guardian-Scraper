b6f73316cad8c1ecf42c224660dadd61356183a7
Mo <moho2010@live.ie>
2025-03-17 15:30:47 -0400

    completed up to part 4

--- main.go
+++ main.go
@@ -1,20 +1,56 @@
 package main
 
-import "fmt"
-
-// userAgents = []string{
-// 	"rest"
-// }
+import ("fmt"
+"github.com/PuerkitoBio/goquery"
+"math/rand"
+"net/http"
+"strings"
+"time"
+)
+userAgents = []string{
+	"rest"
+}
 
 func randomUserAgent(){
-
+ rand.Seed(time.Now().Unix)
+ randomNum := rand.Int() %len(userAgents)
+ return userAgents[randomNum]
 }
 
-func discoverLinks(){
-
+func discoverLinks(response &http.Response, baseURL string) []string{
+	if response != nil {
+		doc, _ := goquery.NewDocumentFromResponse(response)
+		foundUrls := []string{}
+		if doc != nil{
+			doc.Find("a").Each(func(i int, s *goquery.Selection)){
+				res,_ := s.Attr("href")
+				foundUrls = append(foundUrls, res)
+			}
+		}
+		return foundUrls
+	} else{
+		return []string{}
+	}
 }
-func getRequest(){
+func getRequest(targetURL string)(*http.Response, error){
+	client := &http.Client{}
 
+	req,_err := http.NewRequest("GET", targetURL,nil)
+	if err != nil {
+		return nil, err
+	} else{
+		return res, nil
+	}
+
+	req.Header.Set("User-Agent", randomUserAgent)
+
+	res, err := client.Do(req)
+
+	if err != nil {
+		return nil, err
+	} else{
+		return res, nil
+	}
 }
 
 func resolveRelativeLinks(){
@@ -36,8 +72,15 @@ func Crawl(targetURL string, baseURL string) []string{
 			}
 		}
 	}
+	
+	// ParseHTML(response)
 	return foundUrls
 }
+
+// func ParseHTML(response *http.Response){
+	//Can take the information and parse it. maybe use it to get headlines or articles on certain topics? future project idea. 
+	//Tutorial 32 explains parsing results
+// }
 
 func main() {
 	worklist := make(chan []string)
